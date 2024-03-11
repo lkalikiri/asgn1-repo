8168aaa29089c34979a210e4c2500180ac326bf4
lkalikiri <laxmi.kalikiri@gmail.com>
2024-03-11 22:20:22 +0000

    grc.java second commit

--- grc.java
+++ grc.java
@@ -1,5 +1,11 @@
 class grc {
       public static void main(String args[]){
 	      System.out.println("Welcome to the gits training!");
+      int x = 10;
+      int y = 20;
+      int z = x + y;
+      		System.out.println("x = , Y = , Z = ", +x, +y, +z);
+      
       }
+
 }
